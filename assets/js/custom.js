260d0a709337319bf4389f4c64d37bbc9130f5d5
lepisma <abhinav.tushar.vs@gmail.com>
2015-10-21 14:56:44 +0530

    Add contact popup

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,19 @@
 $(document).ready(function(){
     // Header effect
     $("header .measure a").textillate({in: {effect: 'fadeInDownBig', sync: true}});
+
+    // Contact button
+    $("#contact-btn").click(function(){
+        toggleContact();
+    });
+
+    $("#contact-overlay").click(function(){
+        toggleContact();
+    });
 });
+
+
+var toggleContact = function(){
+    $("#contact-overlay").fadeToggle(100);
+    $("#contact-form").slideToggle(100);
+}
