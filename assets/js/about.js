7705159af3915bc8a28eb4c003c88b4e22f2824e
lepisma <abhinav.tushar.vs@gmail.com>
2015-11-11 19:45:51 +0530

    Placeholder homepage

--- assets/js/about.js
+++ assets/js/about.js
@@ -2,9 +2,19 @@
 
 
 $(window).load(function() {
-    $("#notify").typed({
-        strings: ["This page is under construction", "Come back later."],
+    $("#main-title").typed({
+        strings: ["This page is under construction"],
         typeSpeed: 0,
-        loop: true
+        loop: false
     });
+
+    var cssOp = {
+            opacity : 1
+        };
+
+    cssOp["transition"] = "opacity 1s ease-in-out";
+
+    $("#main-subtitle").css(cssOp);
+    $("#main-text").css(cssOp);
+    $(".sidenote").css(cssOp);
 });
