0640b30967857145318b63e0fd6f7c77bbcbe6c8
lepisma <abhinav.tushar.vs@gmail.com>
2015-12-17 02:35:58 +0530

    update about me page

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,20 +1,67 @@
 // About me page scripts
 
-
-$(window).load(function() {
-    $("#main-title").typed({
-        strings: ["This page is under construction"],
-        typeSpeed: 0,
-        loop: false
+$(document).ready(function() {
+    var lastId,
+    menu = $("#menu"),
+    menuHeight = menu.outerHeight() + 15,
+    items = menu.find("a"),
+    scrollItems = items.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
     });
 
-    var cssOp = {
-            opacity : 1
-        };
+    // Bind click handler to menu items
+    $("#links-btn").click(function(ev) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - menuHeight + 1;
+        $("html, body").stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        ev.preventDefault();
+    });
 
-    cssOp["transition"] = "opacity 1s ease-in-out";
+    // Bind click handler to menu items
+    items.click(function(ev) {
+        var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - menuHeight + 1;
+        $("html, body").stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        ev.preventDefault();
+    });
 
-    $("#main-subtitle").css(cssOp);
-    $("#main-text").css(cssOp);
-    $(".sidenote").css(cssOp);
+    // Bind to scroll
+    $(window).scroll(function() {
+
+        // Show/hide menu
+        if ($(this).scrollTop() > menuHeight) {
+            $("#menu").fadeIn();
+        }
+        else {
+            $("#menu").fadeOut();
+        }
+
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + menuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            items
+                .parent().removeClass("active")
+                .end().filter("[href=#" + id + "]").parent().addClass("active");
+        }
+    });
 });
