f34a1290e856200ea595d321327f129aa391dc84
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-04-14 19:47:50 -0400

    Cleanup

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,9 @@
-// Custom js
-
-$(document).ready(function(){
+$(document).ready(function () {
     // Header effect
-    $("header .measure a").textillate({in: {effect: "fadeInDownBig", sync: true}});
-
-    // Contact button
-    $("#contact-btn").click(function(){
-        toggleContact();
-    });
-
-    $("#contact-overlay").click(function(){
-        toggleContact();
-    });
-});
-
-
-var toggleContact = function(){
-    $("#contact-overlay").fadeToggle(100);
-    $("#contact-form").slideToggle(100);
-};
+  $('header .measure a').textillate({
+      in: {
+        effect: 'fadeInDownBig',
+        sync: true
+      }
+  })
+})
