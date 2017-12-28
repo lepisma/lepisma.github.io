b28428532ab492861e3144e76c06baae2a1f6595
Abhinav Tushar <lepisma@fastmail.com>
2017-12-28 00:28:29 -0500

    sympy block

---
+++ _plugins/sympy-block.rb
@@ -0,0 +1,38 @@
+require 'tempfile'
+
+module Jekyll
+  class SympyBlock < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      @tag = markup
+      super
+    end
+
+    def render(context)
+      contents = super
+      Tempfile.open('sympy') do |tmp|
+        lines = contents.split("\n").map do |line|
+          if line.start_with? ":"
+            # This is symbol declaration
+            symbols = line[2..-1]
+            "#{symbols} = symbols('#{symbols}')"
+          elsif line.start_with? ">>"
+            # This is a latex print statement
+            "print(latex(#{line[3..-1]}))"
+          else
+            line
+          end
+        end
+        lines.unshift('init_printing()')
+        lines.unshift('from sympy import *')
+        tmp.write(lines.join("\n"))
+
+        output = `python -c "#{lines.join("\n")}"`
+        "$$ #{output} $$"
+      end
+    end
+
+  end
+end
+
+
+Liquid::Template.register_tag('sympy', Jekyll::SympyBlock)
