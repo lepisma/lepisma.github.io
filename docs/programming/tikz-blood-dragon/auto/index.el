df340399446041168ace31ff7501e619d8299916
Abhinav Tushar <lepisma@fastmail.com>
2018-02-15 19:09:50 -0500

    ups

---
+++ docs/programming/tikz-blood-dragon/auto/index.el
@@ -0,0 +1,47 @@
+(TeX-add-style-hook
+ "index"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem") ("moresize" "11pt")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "tikz"
+    "tikz-3dplot"
+    "float"
+    "xcolor"
+    "moresize"
+    "fontspec")
+   (LaTeX-add-xcolor-definecolors
+    "fpink"
+    "fblue"
+    "fback"
+    "fdark"
+    "forange"
+    "black")
+   (LaTeX-add-fontspec-newfontcmds
+    "stfont"
+    "fcfont"))
+ :latex)
+
--- docs/readings/clippings.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  var epigraph = document.getElementsByClassName('epigraph')[0];
-
-  for (var i = epigraph.children.length; i >= 0; i--) {
-    epigraph.appendChild(epigraph.children[Math.random() * i | 0]);
-  }
-})
