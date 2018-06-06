c619afa27503aa2025cfc2cad576ecc5417137ff
Abhinav Tushar <lepisma@fastmail.com>
2018-06-07 00:15:19 +0530

    Remove local link patch

--- assets/js/local-link-patch.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  [].forEach.call(document.getElementsByTagName('a'), l => {
-    if (l.classList.contains('pile-link')) {
-      l.href = l.href.slice(window.location.origin.length + 5)
-    }
-  })
-})
